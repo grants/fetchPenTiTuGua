51b44e0e67626702e573504604cdf6a802362ba5
qihao <grants@qq.com>
2015-12-10 15:09:29 +0800

    add random

--- test.py
+++ test.py
@@ -5,28 +5,39 @@ from BeautifulSoup import BeautifulSoup
 import requests
 import os
 import re
-number=106633
+import random
+import socket
+socket.setdefaulttimeout(10)
+number=random.randint(106600,106700)
 url="http://dapenti.com/blog/more.asp?name=tupian&id="+str(number)
-webpagesrc=urlopen(url).read()
-uhtml=unicode(webpagesrc,"gbk")
-soup=BeautifulSoup(uhtml)
-srcend=soup.originalEncoding
-ps=soup.findAll('p')
-imgp=ps[2].find("img")
-textp=ps[3]
-imgsrc=imgp['src']
-downloadpicpath="~/camp/pentitugua/"+str(number)+".jpg"
-if os.path.exists(downloadpicpath):
-	pass
-else:
-	os.system("wget "+imgsrc+" -qO "+downloadpicpath+" && convert -resize 300 "+downloadpicpath+" "+downloadpicpath)
-thetext=textp.renderContents()
-print "------------------------------------------------------------------"
-os.system("tycat "+downloadpicpath)
-showtext=re.sub("&nbsp;","",re.sub("<[^>]*?>","",thetext))
-show=""
-for s in showtext.splitlines():
-	s=s.rstrip()
-	show+=s
-print show
-print "------------------------------------------------------------------"
+print
+print "now connecting: "+url
+print
+try:
+	webpagesrc=urlopen(url).read()
+	uhtml=unicode(webpagesrc,"gbk")
+	soup=BeautifulSoup(uhtml)
+	srcend=soup.originalEncoding
+	ps=soup.findAll('p')
+	imgp=ps[2].find("img")
+	textp=ps[3]
+	imgsrc=imgp['src']
+	downloadpicpath="~/camp/pentitugua/"+str(number)+".jpg"
+	if os.path.exists(downloadpicpath):
+		pass
+	else:
+		os.system("wget "+imgsrc+" -qO "+downloadpicpath+
+				" && convert -resize 300 "+downloadpicpath+" "+downloadpicpath)
+		thetext=textp.renderContents()
+	print "------------------------------------------------------------------"
+	os.system("tycat "+downloadpicpath)
+	showtext=re.sub("&nbsp;","",re.sub("<[^>]*?>","",thetext))
+	show=""
+	for s in showtext.splitlines():
+		s=s.rstrip()
+		show+=s
+	print show
+	print "------------------------------------------------------------------"
+except Exception,e:
+	print e
+
