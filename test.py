8a1d68286eb5fc670db50307d6cc8873b1193b2f
qihao <grants@qq.com>
2015-12-10 14:49:02 +0800

    perhaps already done everything I need

--- test.py
+++ test.py
@@ -3,7 +3,10 @@
 from urllib import urlopen
 from BeautifulSoup import BeautifulSoup
 import requests
-url="http://dapenti.com/blog/more.asp?name=tupian&id=106633"
+import os
+import re
+number=106633
+url="http://dapenti.com/blog/more.asp?name=tupian&id="+str(number)
 webpagesrc=urlopen(url).read()
 uhtml=unicode(webpagesrc,"gbk")
 soup=BeautifulSoup(uhtml)
@@ -12,6 +15,18 @@ ps=soup.findAll('p')
 imgp=ps[2].find("img")
 textp=ps[3]
 imgsrc=imgp['src']
-print imgsrc
+downloadpicpath="~/camp/pentitugua/"+str(number)+".jpg"
+if os.path.exists(downloadpicpath):
+	pass
+else:
+	os.system("wget "+imgsrc+" -qO "+downloadpicpath+" && convert -resize 300 "+downloadpicpath+" "+downloadpicpath)
 thetext=textp.renderContents()
-print thetext
+print "------------------------------------------------------------------"
+os.system("tycat "+downloadpicpath)
+showtext=re.sub("&nbsp;","",re.sub("<[^>]*?>","",thetext))
+show=""
+for s in showtext.splitlines():
+	s=s.rstrip()
+	show+=s
+print show
+print "------------------------------------------------------------------"
