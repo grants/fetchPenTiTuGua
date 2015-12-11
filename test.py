eb7a790593a447455f73b0e27e270fc045f8aa3b
qihao <grants@qq.com>
2015-12-11 10:12:11 +0800

    change it

--- test.py
+++ test.py
@@ -11,34 +11,38 @@ import socket
 socket.setdefaulttimeout(10)
 number=random.randint(106600,106700)
 url="http://dapenti.com/blog/more.asp?name=tupian&id="+str(number)
-print
-print "now connecting: "+url
-print
 try:
 	webpagesrc=urlopen(url).read()
 	uhtml=unicode(webpagesrc,"gbk")
 	soup=BeautifulSoup(uhtml)
 	srcend=soup.originalEncoding
 	ps=soup.findAll('p')
-	imgp=ps[2].find("img")
-	textp=ps[3]
-	imgsrc=imgp['src']
-	downloadpicpath="~/camp/pentitugua/"+str(number)+".jpg"
-	if os.path.exists(downloadpicpath):
-		pass
+	for p in ps:
+		imgp=p.find("img")
+		if (imgp):
+			break
+		else:
+			continue
+	if (imgp):
+		imgsrc=imgp['src']
+		downloadpicpath="~/camp/pentitugua/"+str(number)+".jpg"
+		if os.path.exists(downloadpicpath):
+			pass
+		else:
+			os.system("wget "+imgsrc+" -qO "+downloadpicpath+
+					" && convert -resize 300 "+downloadpicpath+" "+downloadpicpath)
+			#thetext=textp.renderContents()
+		print "------------------------------------------------------------------"
+		os.system("tycat "+downloadpicpath)
+		#showtext=re.sub("&nbsp;","",re.sub("<[^>]*?>","",thetext))
+		#show=""
+		#for s in showtext.splitlines():
+		#	s=s.rstrip()
+		#	show+=s
+		#print show
+		print "------------------------------------------------------------------"
 	else:
-		os.system("wget "+imgsrc+" -qO "+downloadpicpath+
-				" && convert -resize 300 "+downloadpicpath+" "+downloadpicpath)
-		thetext=textp.renderContents()
-	print "------------------------------------------------------------------"
-	os.system("tycat "+downloadpicpath)
-	showtext=re.sub("&nbsp;","",re.sub("<[^>]*?>","",thetext))
-	show=""
-	for s in showtext.splitlines():
-		s=s.rstrip()
-		show+=s
-	print show
-	print "------------------------------------------------------------------"
+		print "nothing find"
 except Exception,e:
 	print e
 
